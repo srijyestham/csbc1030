24c3a3873242065eecbd111c431ff62a8ba8e5e9
SriJyestham <sriindram18@gmail.com>
2023-12-11 02:36:15 -0500

    Assignment 2 Final Commit

---
+++ assignment2/cjs/calculate.cjs
@@ -0,0 +1,6 @@
+// CommonJS File
+const calculateArea = (length, width) => length * width;
+const calculateVolume = (length, width, height) => length * width * height;
+const calculateSquare = (length) => length * length;
+
+module.exports = {calculateArea, calculateVolume, calculateSquare};
---
+++ assignment2/esm/calculate.mjs
@@ -0,0 +1,8 @@
+// ES6 File
+let calculateArea = (length, width) => length * width;
+
+let calculateVolume = (length, width, height) => length * width * height;
+
+let calculateSquare = (length) => length * length;
+
+export { calculateArea, calculateVolume, calculateSquare }
